e97120069f00f21d433e878d80b40be5b3202b66
AbrorAzizov <houston.070224@gmail.com>
2025-07-21 10:41:39 +0500

    Auth & home on process

---
+++ lib/features/auth/bloc/auth_cubit.dart
@@ -0,0 +1,52 @@
+import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:qolber_clean_arc/features/auth/bloc/auth_state.dart';
+
+import '../../../servise_locator.dart';
+import '../data/models/sign_up_parameters.dart';
+import '../data/repository/auth_repo_imp.dart';
+import '../domain/repo/auth_repo.dart';
+
+class AuthCubit extends Cubit<AuthState> {
+  AuthCubit() : super (AuthStateInitial());
+
+
+  Future<void> initialize() async {
+    final isLoggedIn = await sl<AuthRepo>().isLoggedIn();
+    if (isLoggedIn) {
+      emit(AuthStateLoggedIn());
+    } else {
+      emit(AuthStateSignup());
+    }
+  }
+
+
+  Future<void> sigUp(UserModel user) async {
+    emit(AuthStateLoading());
+    try {
+      Either response = await AuthRepoImp().signUp(user);
+      response.fold((error) {
+        emit(AuthStateError(authError: error));
+      }, (data) {
+        emit(AuthStateLoggedIn());
+      });
+    } catch (e) {
+      emit(AuthStateError(authError: "$e"));
+    }
+  }
+
+  Future<void> signIn(UserModel userModel) async {
+    emit(AuthStateLoading());
+    try {
+      Either response = await AuthRepoImp().signIn(userModel);
+      response.fold((error) {
+        emit(AuthStateError(authError: error));
+      }, (data) {
+        emit(AuthStateLoggedIn());
+      });
+    } catch (e) {
+      emit(AuthStateError(authError: "$e"));
+    }
+  }
+
+}
---
+++ lib/features/auth/domain/repo/auth_repo.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+import '../../data/models/sign_up_parameters.dart';
+
+abstract class AuthRepo {
+  Future<Either> signIn (UserModel user);
+  Future<Either> signUp (UserModel user);
+  Future<bool> isLoggedIn ();
+  Future<Either> getUser();
+  Future<Either> logout();
+}
